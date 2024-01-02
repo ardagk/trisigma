3e8cdd80a47253f87c3a1a0fb48025139d747e0b
Arda Gök <ardagk@Ardas-Air.lan>
2024-01-01 22:47:12 -0600

    electron-forge and publisher configs added

---
+++ clients/pms/forge.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'ardagk',
+          name: 'trisigma'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
